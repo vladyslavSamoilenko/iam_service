3b002875d49a0e45ab4b198bffb45b4ee6067878
dusha <dushasam26@gmail.com>
2025-06-01 22:38:01 +0200

    add v1__init file

---
+++ iam_service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE posts(
+    id BIGSERIAL PRIMARY KEY ,
+    title varchar(255) UNIQUE NOT NULL,
+    content TEXT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    likes INTEGER NOT NULL DEFAULT 0
+);
+
+INSERT INTO posts(title, content,created,likes) values ('First post', 'This is first post', CURRENT_TIMESTAMP,10),
+                                                       ('Second post', 'This is second post', CURRENT_TIMESTAMP,3);
