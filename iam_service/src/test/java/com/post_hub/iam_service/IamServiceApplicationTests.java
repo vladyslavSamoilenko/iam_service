4b44610b26858b2241d1cf4cd949355420da39c2
dusha <dushasam26@gmail.com>
2025-06-01 17:04:37 +0200

    init commit

---
+++ iam_service/src/test/java/com/post_hub/iam_service/IamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post_hub.iam_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
